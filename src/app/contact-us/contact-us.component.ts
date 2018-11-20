17e2406b5d306d7bf48aef3001d92900e3dce599
y0dev <omgitszslick@gmail.com>
2018-11-19 19:38:41 -0800

    change url to appropriate url

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
-import { MessageService } from '../message.service';
-// installed using npm install --save rxjs-compat
-
-import { Contact } from '../contact';
-import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -14,9 +9,8 @@ import { ContactService } from '../contact.service';
 })
 export class ContactUsComponent implements OnInit {
 
-  readonly ROOT_URL = "https://my-json-server.typicode.com/y0dev/json_server/posts/";
+  readonly ROOT_URL = "interview-contact-submit-api-lb-1009699934.us-east-1.elb.amazonaws.com";
 
-  contacts: Contact[];
   email: string;
   subject: string;
   body: string;
@@ -25,8 +19,7 @@ export class ContactUsComponent implements OnInit {
   submitted: boolean = false;
   success: boolean = false;
 
-  constructor(private formBuilder: FormBuilder,
-              private contactService: ContactService) {
+  constructor(private formBuilder: FormBuilder,private http: HttpClient) {
     this.messageForm = this.formBuilder.group({
       email: ['',Validators.required],
       subject: ['',Validators.required],
@@ -41,44 +34,29 @@ export class ContactUsComponent implements OnInit {
     this.email = this.messageForm.controls.email.value
     this.subject = this.messageForm.controls.subject.value
     this.body = this.messageForm.controls.body.value
-    this.add()
-    // this.postContactInfo()
+
+    this.postContactInfo()
   }
 
-  // postContactInfo() {
-  //   this.http.post(this.ROOT_URL,
-  //   {
-  //     email: this.email,
-  //     subject: this.subject,
-  //     body: this.body
-  //   }).subscribe(
-  //     (data:any) => {
-  //       this.success = true;
-  //       this.status = data['status']
-  //       console.log(data)
-  //     }, error => {
-  //       this.status = error['status']
-  //       console.log(this.status)
-  //     } // error path
-  //   )
-  // }
+  postContactInfo() {
+    this.http.post(this.ROOT_URL,
+    {
+      email: this.email,
+      subject: this.subject,
+      body: this.body
+    }).subscribe(
+      (data:any) => {
+        this.success = true;
+        this.status = data['status']
+        console.log(data)
+      }, error => {
+        this.status = error['status']
+        console.log(this.status)
+      } // error path
+    )
+  }
 
   ngOnInit() {
-    this.getContacts();
-  }
-
-  add(): void {
-    console.log(this.contacts)
-    this.contactService.addContact({ email:this.email,subject:this.subject,body:this.body } as Contact)
-      .subscribe(contact => {
-        this.contacts.push(contact);
-        this.success = true;
-      });
-  }
-
-  getContacts(): void {
-    this.contactService.getContacts()
-    .subscribe(contacts => this.contacts = contacts);
   }
 
 }
